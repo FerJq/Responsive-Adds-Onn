835f2907af90c5debe4c6775c938f556d3739031
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-03-16 23:09:17 -0400

    images loop

---
+++ src/app.js
@@ -0,0 +1,39 @@
+let random = [1, 2, 3, 4];
+
+//slice-one
+let img_one = document.querySelector(".section-2");
+setInterval(function () {
+  let number = Math.floor(Math.random() * 4);
+  let images = random[number];
+  img_one.innerHTML = `            <img
+              src="image/slice-one/${[images]}.jpg"
+              alt=""
+              class="img-fluid"
+            />`;
+}, 6000);
+//slice-two
+let img_two = document.querySelector(".galery-1");
+setInterval(function () {
+  let number = Math.floor(Math.random() * 4);
+  let images = random[number];
+  img_two.innerHTML = `<a href="https://www.pexels.com/search/husky"
+            ><img
+              src="image/slice-two/${[images]}.jpg"
+              alt=""
+              class="img-fluid"
+          /></a>`;
+}, 6000);
+
+//slice-three
+let img_three = document.querySelector(".galery-2");
+setInterval(function () {
+  let number = Math.floor(Math.random() * 4);
+  let images = random[number];
+  img_three.innerHTML = `
+          <a href="https://www.pexels.com/search/husky"
+            ><img
+              src="image/slice-three/${[images]}.jpg"
+              alt=""
+              class="img-fluid"
+          /></a>`;
+}, 6000);
