c53f09fb0722414eadcd5feab8ee73981867dc37
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-03-17 01:34:14 -0400

    navbar href

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+//background
+
+let bg = document.querySelector("body");
+
+function background_theme() {
+  if (bg.classList.contains("color-theme")) {
+    bg.classList.remove("color-theme");
+  } else {
+    bg.classList.add("color-theme");
+  }
+}
+
+setInterval(background_theme, 3000);
+
+///
 let random = [1, 2, 3, 4];
 
 //slice-one
